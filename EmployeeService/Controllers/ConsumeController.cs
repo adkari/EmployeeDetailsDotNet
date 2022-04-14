a6ba07b5b6847e5a734ceb93e2d123c3cca6b2f0
sakxyam adhikari <sakxyamadk@gmail.com>
2022-04-14 21:01:23 +0545

    Initial Commit

---
+++ EmployeeService/Controllers/ConsumeController.cs
@@ -0,0 +1,85 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    public class ConsumeController : Controller
+    {
+        // GET: Consume
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<SW_TBL_EMPLOYEE> list=new List<SW_TBL_EMPLOYEE>();
+            client.BaseAddress = new Uri("https://localhost:44361/api/NewApi");
+            var response = client.GetAsync("NewApi");
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<SW_TBL_EMPLOYEE>>();
+                display.Wait();
+                list = display.Result;
+            }
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(SW_TBL_EMPLOYEE emp)
+        {
+            client.BaseAddress = new Uri("https://localhost:44361/api/NewApi");
+            var response = client.PostAsJsonAsync<SW_TBL_EMPLOYEE>("NewApi", emp);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+
+        
+        public ActionResult Edit(int id)
+        {
+            SW_TBL_EMPLOYEE e = null;
+            client.BaseAddress = new Uri("https://localhost:44361/api/NewApi");
+            var response = client.GetAsync("NewApi?id="+id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<SW_TBL_EMPLOYEE>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(SW_TBL_EMPLOYEE e)
+        {
+            client.BaseAddress = new Uri("https://localhost:44361/api/NewApi");
+            var response = client.PutAsJsonAsync<SW_TBL_EMPLOYEE>("NewApi", e);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+    }
+}
---
+++ EmployeeService/Controllers/NewApiController.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    public class NewApiController : ApiController
+    {
+        EmployeeDbEntities db = new EmployeeDbEntities();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Action()
+        {
+            var obj = db.crudEmp(0, "", 0, "Get").ToList();
+            return Ok(obj);
+        }
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetEmployeeById(int id)
+        {
+            var emp = db.SW_TBL_EMPLOYEE.Where(model => model.EmpId == id).FirstOrDefault();
+            return Ok(emp);
+        }
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult EmpInsert(SW_TBL_EMPLOYEE e)
+        {
+            SqlParameter param1 = new SqlParameter("@EmpName", e.EmpName);
+            SqlParameter param2 = new SqlParameter("@Salary", e.Salary);
+            db.Database.ExecuteSqlCommand("crudEmp 0,@EmpName,@Salary,'Insert'",param1,param2);
+            return Ok();
+
+        }
+
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult EmpUpdate(SW_TBL_EMPLOYEE e)
+        {
+            SqlParameter param1 = new SqlParameter("@EmpName", e.EmpName);
+            SqlParameter param2 = new SqlParameter("@Salary", e.Salary);
+            SqlParameter param3 = new SqlParameter("@EmpId", e.EmpId);
+            db.Database.ExecuteSqlCommand("crudEmp @EmpId,@EmpName,@Salary,'Update'",param3, param1, param2);
+            return Ok();
+        }
+    }
+}
